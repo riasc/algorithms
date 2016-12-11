e015ba1f2e22a171b5fdc88fdad0c680773bc7d9
riasc <richard.schaefer@ibvt.uni-stuttgart.de>
2016-12-12 00:03:03 +0100

    added some lines

---
+++ search algorithms/binary search/binary_search.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+import sys #
+import math #
+
+__author__ = "Richard A. Schaefer"
+__license__ = "GPL"
+__email__ = "richard.schaefer@zoho.com"
+
+# binary search for element in list
+def binary_search(list, element):
+
