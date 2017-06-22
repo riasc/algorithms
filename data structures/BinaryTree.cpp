07cdae971eb9628450ea70c4dcce12e5b4f2090f
riasc <richard.schaefer@ibvt.uni-stuttgart.de>
2017-06-22 20:29:03 +0200

    search

--- data structures/BinaryTree.cpp
+++ data structures/BinaryTree.cpp
@@ -46,68 +46,108 @@ void BinaryTree::set_root(Node * rootnode) {
 
 void BinaryTree::preorder_tree_walk(Node *startnode) {
   if ( startnode != NULL ) {
-    cout << startnode->key << endl;
+    cout << "key: " << startnode->key << endl;
     preorder_tree_walk(startnode->left);
     preorder_tree_walk(startnode->right);
   }
 }
 
 void BinaryTree::inorder_tree_walk(Node *startnode) {
+  if ( startnode != NULL ) {
+    preorder_tree_walk(startnode->left);
+    cout << "key: " << startnode->key << endl;
+    preorder_tree_walk(startnode->right);
+  }
 }
 
 void BinaryTree::postorder_tree_walk(Node *startnode) {
+  if ( startnode != NULL ) {
+    preorder_tree_walk(startnode->left);
+    preorder_tree_walk(startnode->right);
+    cout << "key: " << startnode->key << endl;
+  }
 }
 
+// insert new node into binary tree
 void BinaryTree::insert(Node *insertnode) {
-  Node * buf = NULL; // buffering nodes
-  Node * element = get_root(); // start with root
-  while ( root != NULL ) {
-    buf = root; // 
-    if ( insertnode->key < element->key ) {
-      element = element->left;
+  Node * buff = NULL; // buffering nodes
+  Node * elem  = get_root(); // start with provided root
+  while ( elem != NULL ) { // 
+    buff = elem; // 
+    if ( insertnode->key < elem->key ) {
+      elem = elem->left;
     } else {
-      element = element->right; 
+      elem = elem->right; 
     }
   }
-  if ( buf == NULL ) {
+  if ( buff == NULL ) {
+    cout << "binary tree is empty - set root" << endl;
     set_root(insertnode);
   } else {
-    if ( insertnode->key < buf->key ) {
-      buf->left = insertnode; 
+    if ( insertnode->key < buff->key ) {
+      buff->left = insertnode; 
     }
     else {
-      buf->right = insertnode; 
+      buff->right = insertnode; 
     }
   }
 }
 
-/*Node* BinaryTree::search(int searchvalue) {
-}*/
+// search for value k starting with root x
+Node* BinaryTree::search(Node * x, int k) {
+  if ( x == NULL ) || ( k == x->key ) {
+    return root; 
+  }
+  if ( k < x->key ) {
+    return search( x->left, k); 
+  }
 
+
+}
 
 int main() {
   BinaryTree bt; // create new binary tree object
   int menu = 0; // 
-  int key;
-  
-  Node tempnode;
-  Node * ptrnode = &tempnode;
+  Node * temp;
 
-  while (menu != 9) {
+  while (1) {
+    cout << endl<<endl;
     cout << "(1) - insert node" << endl;
     cout << "(2) - print tree (preorder tree walk)" << endl;
+    cout << "(3) - print tree (inorder tree walk)" << endl;
+    cout << "(4) - print tree (postorder tree walk)" << endl;
+    cout << "(5) - search for key" << endl;
     cout << "(9) - exit" << endl;
+   
+    cout << "please make your selection ";
     cin >> menu;
     switch(menu) {
-      case 1: cout << "key to insert ";
-              cin >> key; // get key input from user
-              ptrnode->key = key; // add inserted key
-              bt.insert(ptrnode);
+      case 1: temp = new Node;
+              cout << "key to insert ";
+              cin >> temp->key; // get key input from user
+              bt.insert(temp);
+              cout << "root" << bt.get_root()->key << endl;
               break;
-      case 2: bt.preorder_tree_walk(bt.get_root());
+
+      case 2: cout << "start traversel (preorder tree walk)" << endl; 
+              bt.preorder_tree_walk(bt.get_root());
               break;
+
+      case 3: cout << "start traversel (inorder tree walk)" << endl; 
+              bt.inorder_tree_walk(bt.get_root());
+              break;
+      
+      case 4: cout << "start traversel (postorder tree walk)" << endl; 
+              bt.postorder_tree_walk(bt.get_root());
+              break;
+
+      case 5: cout << "search key" << endl;
+              break;
+
       case 9: cout << "exit program - e called" << endl;
               exit(0);
+              break;
+
     }
   }
 }
